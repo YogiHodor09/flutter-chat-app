76b5474c28896af9bb8fee9c344ce5321becf879
YogiPC\Yogeshwar <yogihodor09@outlook.com>
2022-05-23 09:22:16 +0530

    Combat chat app initial commit

---
+++ build/cometchat/generated/source/buildConfig/debug/com/dart/cometchat/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dart.cometchat;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.dart.cometchat";
+  public static final String BUILD_TYPE = "debug";
+}
