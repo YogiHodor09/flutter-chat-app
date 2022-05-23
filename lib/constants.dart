dde77be588a04bc5089dcf2f32bff0b6ed1255fb
YogiPC\Yogeshwar <yogihodor09@outlook.com>
2022-05-23 09:16:49 +0530

    Constant file credentials changes

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,10 @@
 import 'package:intl/intl.dart';
 
 class CometChatAuthConstants {
-  static const String appId = "APPID";
-  static const String authKey = "AUTHKEY";
-  static const String region = "REGION";
-  static const String apiKey = "APIKEY";
+  static const String appId = "210273dbddc1c8b7";
+  static const String authKey = "ddbbe676b72f076d0c0b2ecba37ccfc86f9da946";
+  static const String region = "us";
+  static const String apiKey = "44c76ee5255995110e37bc10c319902a505c37c1";
 }
 
 String USERID = "";
